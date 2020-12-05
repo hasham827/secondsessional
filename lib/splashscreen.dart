634bf5a3f0af4e0fb7a7686e48f65c85413ed76e
Hasham Ahmad <69302740+hasham827@users.noreply.github.com>
2020-12-05 12:25:30 +0500

    Add SplashScreen and add dependencies

---
+++ lib/splashscreen.dart
@@ -0,0 +1,59 @@
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+//import 'home.dart';
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+       // builder: (context) => HomePage(),
+      ));
+    });
+  }
+
+  // added test yourself
+  // and made the text to align at center
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body:
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset('assets/images/GeeksforGeeks.jpg'),
+                decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                      image: new AssetImage("images/me.jpeg"),
+                      fit: BoxFit.fill,
+                    )
+                )
+            ),
+            Container(
+              child: Text(
+              "Hasham Ahmad\nSP17-BCS-015",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+                fontFamily: "Satisfy",
+              ),textAlign: TextAlign.center,
+            ),
+            )
+          ],
+
+
+        ),
+      ),
+    );
+  }
+}
