738f0260e2d26b3e938aa64ce68696e1fbb12222
Hasham Ahmad <69302740+hasham827@users.noreply.github.com>
2020-12-05 16:16:12 +0500

    adding score card

--- lib/simple.dart
+++ lib/simple.dart
@@ -85,6 +85,22 @@ class _HomePageState extends State<HomePage3> {
             child: Text(
                 " Score of A ",style: TextStyle(fontSize:25, color: Colors.white, fontWeight:FontWeight.bold)
             ),
+
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 40.0),
+            child: Column(
+              children:[
+                Card(
+                  child: ListTile(
+                    //contentPadding: const EdgeInsets.all(16.0),
+                    title: Text("Score :" ,style: TextStyle(fontSize:20, color: Colors.purple, fontWeight:FontWeight.bold)),
+                  ),
+                ),
+
+              ],
+
+            )
           ),
           Container(
             margin: EdgeInsets.only(left: 200,top: 10.0),
@@ -92,31 +108,148 @@ class _HomePageState extends State<HomePage3> {
                 " Score of B ",style: TextStyle(fontSize:25, color: Colors.white, fontWeight:FontWeight.bold)
             ),
           ),
-      Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+          Container(
+            child: Column(
 
-              Image(
-                image: diceimage,
-                width: 100.0,
-                height: 100.0,
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 40.0),
-                child: RaisedButton(
-                  color: Colors.yellow,
-                  padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
-                  child: Text(
-                    'Roll the dice!',
-                  ),
-                  onPressed: rollDice,
-                ),
-              )
-            ],
+
+    )
+
           ),
-        ),
-      )]));
+          Container(
+            margin: EdgeInsets.only(left: 270.0,top: 600),
+            decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                shape: BoxShape.circle
+            ),
+            height: 60,
+            width: 60.0,
+            padding: EdgeInsets.all(5.0),
+            child: FlatButton(
+              onPressed: () => {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                )
+                ),
+              },
+              padding: EdgeInsets.all(5.0),
+              child: Column( // Replace with a Row for horizontal icon + text
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+                  Icon(FontAwesomeIcons.home,color: Colors.white,size: 30,),
+
+                  SizedBox(height: 5,),
+
+
+                ],
+              ),
+            ),
+
+          ),
+      Container(
+            margin: EdgeInsets.only(right:200,top: 260),
+
+            child: Center(
+
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column( // Replace with a Row for horizontal icon + text
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image(
+                        image: diceimage,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+
+                      SizedBox(height: 5,),
+                      Container(
+                        margin: EdgeInsets.only(top: 40.0),
+                        child: RaisedButton(
+                          color: Colors.purpleAccent,
+                          padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+                          child: Text(
+                            'Roll the dice A',
+                          ),
+                          onPressed: rollDice,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 270.0,top: 200),
+                    decoration: BoxDecoration(
+                        color: Colors.purpleAccent,
+                        shape: BoxShape.circle
+                    ),
+                    height: 60,
+                    width: 60.0,
+                    padding: EdgeInsets.all(5.0),
+                    child: FlatButton(
+                      onPressed: () => {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => HomePage(),
+                        )
+                        ),
+                      },
+                      padding: EdgeInsets.all(5.0),
+                      child: Column( // Replace with a Row for horizontal icon + text
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+
+                          Icon(FontAwesomeIcons.home,color: Colors.white,size: 30,),
+
+                          SizedBox(height: 5,),
+
+
+                        ],
+                      ),
+                    ),
+
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(left:200,top: 260),
+
+            child: Center(
+
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column( // Replace with a Row for horizontal icon + text
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image(
+                        image: diceimage,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+
+                      SizedBox(height: 5,),
+                      Container(
+                        margin: EdgeInsets.only(top: 40.0),
+                        child: RaisedButton(
+                          color: Colors.purpleAccent,
+                          padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+                          child: Text(
+                            'Roll the dice B',
+                          ),
+                          onPressed: rollDice,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+          )
+
+        ]));
   }
 }
